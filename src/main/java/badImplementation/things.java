2491f977a8c730140924eeae1150bfd5b1dd4650
Susmita Poudel <susmita.poudel@centre.edu>
2024-02-13 21:06:45 -0500

    Made tests, fixed bugs, and added comments.

--- src/main/java/badImplementation/things.java
+++ src/main/java/badImplementation/things.java
@@ -2,42 +2,60 @@ package badImplementation;
 import java.util.ArrayList;
 import org.w3c.dom.html.HTMLTableRowElement;
 public class things {
-ArrayList<Double> w;
-ArrayList<Double> h;
+ArrayList<Double> hours;
+ArrayList<Double> wage;
 ArrayList<String> name;
+ArrayList<String> people;
 public things() {
-w = new ArrayList<Double>();
-h = new ArrayList<Double>();
-name = new ArrayList<String>();}
-public void add(double a, double b, String c) {
-w.add(a);
-h.add(b);
-name.add(c);}
+	/*
+	 * This is a counter for the number of hours spent refactoring this code. When you look at this code
+	 * and are unable to fix it, add to the counter and submit without changes.
+	 * 
+	 * Current counter: 402
+	 */
+this.hours = new ArrayList<Double>();
+this.wage = new ArrayList<Double>();
+this.name = new ArrayList<String>();
+this.people = new ArrayList<String>();}
+public void add(String p, double a, double b, String c) {
+this.people.add(p);
+this.hours.add(a);
+this.wage.add(b);
+this.name.add(c);}
 public void set(int i, double a, boolean yes) {
 if(yes)
-h.set(i, a);
+this.wage.set(i, a);
 if(!yes)
-w.set(i, a);}
+this.hours.set(i, a);}
 public double[] money() {
-int error = 0;
+	//whether code returned successfully
+	int error = 0;
 double $[];
+//luv slayyyy
 ArrayList<Double> t = new ArrayList<Double>();
-$ = new double[h.size()];
-for(int slay = t.size()-1; slay >= 0; slay--) {
-if(name.get(slay).equals("Contractor"))
-t.add(h.get(slay)*w.get(slay)*2/2);
-if(name.get(slay).equals("Salary"))
-t.add(w.get(slay)*40*1*1*1);
-if(name.get(slay).equals("Hourly"))
-t.add(w.get(slay)*40+w.get(slay)*(h.get(slay)-40)*1.5 *((h.get(slay) > 40)? 1:0)+w.get(slay)*h.get(slay)*((h.get(slay) <= 40)? 1:0));
-if(name.get(slay).equals("contractor"))
-t.add(h.get(slay)*w.get(slay)*2/2);
-if(name.get(slay).equals("salary"))
-t.add(w.get(slay)*40*1*1*1);
-if(name.get(slay).equals("Hourly"))
-t.add(w.get(slay)*40+w.get(slay)*(h.get(slay)-40)*1.5 *((h.get(slay) > 40)? 1:0)+w.get(slay)*h.get(slay)*((h.get(slay) <= 40)? 1:0));
+$ = new double[this.hours.size()];
+for(int slay = this.hours.size()-1; slay >= 0; slay--) {
+if(this.name.get(slay).equals("Contractor"))
+t.add(this.wage.get(slay)*this.hours.get(slay)*2/2);
+if(this.name.get(slay).equals("Salary"))
+t.add(this.hours.get(slay)*40*1*1*1);
+if(this.name.get(slay).equals("Hourly"))
+{
+if(wage.get(slay) > 40) t.add((this.wage.get(slay) - 40.0) * this.hours.get(slay) * 1.5 + 40.0 * this.hours.get(slay));
+else t.add(this.wage.get(slay) * this.hours.get(slay));
+	
 }
-int thesize = t.size();
+//t.add(this.w.get(slay)*40+this.w.get(slay)*(this.h.get(slay)-40)*1.5 *((this.h.get(slay) > 40)? 1:0) + this.w.get(slay)*this.h.get(slay)*((this.h.get(slay) <= 40)? 1:0));
+if(this.name.get(slay).equals("contractor"))
+	//for case
+t.add(this.wage.get(slay)*this.hours.get(slay)*2/2);
+if(this.name.get(slay).equals("salary"))
+	//for cases
+t.add(this.hours.get(slay)*40*1*1*1);
+if(this.name.get(slay).equals("hourly"))
+t.add(this.hours.get(slay)*40+this.hours.get(slay)*(this.wage.get(slay)-40)*1.5 *((this.wage.get(slay) > 40)? 1:0)+this.hours.get(slay)*this.wage.get(slay)*((this.wage.get(slay) <= 40)? 1:0));
+}
+int thesize = t.size(); //the size of t
 for(error = 0; t.size()>0;error++){
 	$[thesize - t.size()] = t.get(t.size() - 1);
 	t.remove(t.size() - 1);}
--- src/test/java/badImplementation/ThingsTest.java
+++ src/test/java/badImplementation/ThingsTest.java
@@ -5,15 +5,79 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ThingsTest {
-
+class thingsTest
+{
+	things wage_employee;
+	things wage_employee_overtime;
+	things salary_employee;
+	things contract_employee;
+	things employee_set;
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws Exception
+	{
+		wage_employee = new things();
+		wage_employee.add("Alice", 10.0,20.0, "Hourly");
+		wage_employee_overtime = new things();
+		wage_employee_overtime.add("Bob", 10.0,50.0, "Hourly");
+
+		salary_employee = new things();
+		salary_employee.add("Charlie", 10.0,50.0,"Salary");
+		contract_employee = new things();
+		contract_employee.add("David", 10.0, 50.0, "Contractor");
+		
+		employee_set = new things();
+		employee_set.add("Alice", 10.0,20.0, "Hourly");
+		employee_set.add("Bob", 10.0,50.0, "Hourly");
+		employee_set.add("Charlie", 10.0,50.0,"Salary");
+		employee_set.add("David", 10.0, 50.0, "Contractor");
+
+		
+
+		
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWage()
+	{
+		double[] expected_amount = new double[1];
+		expected_amount[0] = 20.0 * 10.0;
+		assertEquals(wage_employee.money()[0], expected_amount[0]);
+		double[] expected_ot = new double[1];
+		expected_ot[0] = 40.0 * 10.0 + 10.0 * 10.0 * 1.5;
+		assertEquals(wage_employee_overtime.money()[0], expected_ot[0]);
 	}
+
+	@Test
+	void testSalary()
+	{
+		double[] salary_amt = new double[1];
+		salary_amt[0] = 40.0*10.0;
+	    assertEquals(salary_employee.money()[0], salary_amt[0]);
+	}
+	
+	@Test
+	void testContractor()
+	{
+		double[] contract_amt = new double[1];
+		contract_amt[0] = 50.0*10.0;
+	    assertEquals(contract_employee.money()[0], contract_amt[0]);
+	}
+	
+	@Test
+	void testAll()
+	{
+		double[] expected_amounts = new double[4];
+		expected_amounts[0] = 20.0 * 10.0;
+		expected_amounts[1] = 40.0 * 10.0 + 10.0 * 10.0 * 1.5;
+		expected_amounts[2] = 40.0 * 10.0;
+		expected_amounts[3] = 50.0 * 10.0;
+		
+		double[] returned_amounts = employee_set.money();
+		for(int i=0;i<returned_amounts.length; i++)
+		{
+			assertEquals(expected_amounts[i], returned_amounts[i]);
+		}
+	}
+	
 
 }
